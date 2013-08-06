57aa746cee88e31062f4e5b14c81f67be52c002a
Martin Bean <martin@martinbean.co.uk>
2013-08-06 16:56:59 +0100

    Fixed options after updated them

--- View/Helper/GravatarHelper.php
+++ View/Helper/GravatarHelper.php
@@ -6,15 +6,16 @@ class GravatarHelper extends AppHelper {
     public $helpers = array('Html');
     
     public function image($email, $options = array()) {
-        $options = array_merge(array(
+        $defaults = array(
             'size' => 80,
             'default' => 'mm',
             'rating' => 'g'
-        ), $options);
+        );
+        $options = array_merge($defaults, $options);
         
         $url = 'http://www.gravatar.com/avatar/';
         $url.= md5($email);
-        $url.= sprintf('?s=%d&d=%s&r=%s', $size, $default, $rating);
+        $url.= sprintf('?s=%d&d=%s&r=%s', $options['size'], $options['default'], $options['rating']);
         
         return $this->Html->image($url, $options);
     }
