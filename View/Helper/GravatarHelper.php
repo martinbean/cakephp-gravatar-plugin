0461b1f82c7639d7df2334a4e8291e518f7b9638
Martin Bean <martin@martinbean.co.uk>
2013-08-06 16:55:59 +0100

    Tidied up options

--- View/Helper/GravatarHelper.php
+++ View/Helper/GravatarHelper.php
@@ -6,22 +6,11 @@ class GravatarHelper extends AppHelper {
     public $helpers = array('Html');
     
     public function image($email, $options = array()) {
-        $size = 80;
-        $default = 'mm';
-        $rating = 'g';
-        
-        if (isset($options['size'])) {
-            $size = intval($options['size']);
-            unset($options['size']);
-        }
-        if (isset($options['default'])) {
-            $default = $options['default'];
-            unset($options['default']);
-        }
-        if (isset($options['rating'])) {
-            $rating = $options['rating'];
-            unset($options['rating']);
-        }
+        $options = array_merge(array(
+            'size' => 80,
+            'default' => 'mm',
+            'rating' => 'g'
+        ), $options);
         
         $url = 'http://www.gravatar.com/avatar/';
         $url.= md5($email);
